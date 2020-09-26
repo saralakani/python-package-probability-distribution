38c35ffa941edc4aab50f316e33d4b6a6ab7dad2
saralakani <saralakani@users.noreply.github.com>
2020-09-26 02:42:52 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='Sara_distributions_udacity',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      packages=['Sara_distributions_udacity'],
+      zip_safe=False)
